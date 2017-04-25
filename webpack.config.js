b56889b8ecf76260cb94bbc13ece0d17eb44d2e8
Rafał Bielec <accounts@rbdevelopment.pl>
2017-04-25 23:59:33 +0200

    Set up eslintrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,42 @@
+const path = require('path');
+
 module.exports = {
-    entry: ['./app/index.js', './app/app.scss'],
+    entry: ['./app/index.jsx', './app/app.scss'],
     output: {
-        path: __dirname + '/build',
-        filename: 'bundle.js',
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.json', '.js', '.jsx', '.scss']
     },
     module: {
         loaders: [
             {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_module/,
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                }
             },
             {
                 test: /\.scss$/,
                 exclude: /(node_modules|bower_components)/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-        ],
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
     },
+    preLoaders: [
+        {
+            test: /\.js$/,
+            loaders: ['eslint'],
+            include: [new RegExp(path.join(__dirname, 'app'))]
+        }
+    ],
     devServer: {
         port: 3000,
         contentBase: './build',
-        inline: true,
+        inline: true
     },
+    devtool: 'source-map'
 };
