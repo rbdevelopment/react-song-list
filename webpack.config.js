41ace653576438e7336ce62b50b1736820d63475
Rafał Bielec <accounts@rbdevelopment.pl>
2017-04-25 20:35:18 +0200

    Arranged basic layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: ['./app/index.js'],
+    entry: ['./app/index.js', './app/app.scss'],
     output: {
         path: __dirname + '/build',
         filename: 'bundle.js',
@@ -10,6 +10,11 @@ module.exports = {
                 loader: 'babel-loader',
                 test: /\.js$/,
                 exclude: /node_module/,
+            },
+            {
+                test: /\.scss$/,
+                exclude: /(node_modules|bower_components)/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader'],
             },
         ],
     },
