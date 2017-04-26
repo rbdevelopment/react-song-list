ffa7ca1cc0bd29d9094562d02b7c36c728a59cb3
Rafał Bielec <accounts@rbdevelopment.pl>
2017-04-26 18:50:06 +0200

    Added floating drop down menu.

--- app/components/Playlists.jsx
+++ app/components/Playlists.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Playlists = () => (
-    <div>Playlists</div>
+    <div className="dropdown">
+        <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">select playlist <span className="caret"></span>
+        </button>
+        <ul className="dropdown-menu dropdown-show" aria-labelledby="dropdownMenu1">
+            <li><a href="#">Wedding songs</a></li>
+            <li><a href="#">Pub gig</a></li>
+        </ul>
+    </div>
 );
 
 export default Playlists;
