7e82959efdc2f7bbea4b9af7f0969c3a3b401b83
Rafał Bielec <accounts@rbdevelopment.pl>
2017-04-26 13:41:54 +0200

    Added bootstrap-react-15

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 
 ReactDOM.render(
-  <div>It works!</div>,
+  <DropdownButton bsStyle="primary" title="Example Boostrap Button">
+    <MenuItem eventKey="1">Action</MenuItem>
+    <MenuItem eventKey="2">Another action</MenuItem>
+    <MenuItem eventKey="3">Active Item</MenuItem>
+  </DropdownButton>,
   document.getElementById('app')
 );
