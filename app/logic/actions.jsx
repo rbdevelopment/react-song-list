e0790e48ebb6d1d35b8867523539a4f289af594c
Rafał Bielec <accounts@rbdevelopment.pl>
2017-04-26 17:10:56 +0200

    Added unit tests for all Redux actions.

--- app/logic/actions.jsx
+++ app/logic/actions.jsx
@@ -6,3 +6,27 @@ export const addSong = (title, artist, year) => ({
   artist,
   year
 });
+export const removeSong = (title, artist, year) => ({
+  type: constants.REMOVE_SONG,
+  title,
+  artist,
+  year
+});
+export const removeSongGlobally = (title, artist, year) => ({
+  type: constants.REMOVE_SONG_GLOBALLY,
+  title,
+  artist,
+  year
+});
+export const addPlaylist = (name) => ({
+  type: constants.ADD_PLAYLIST,
+  name
+});
+export const selectPlaylist = (name) => ({
+  type: constants.SELECT_PLAYLIST,
+  name
+});
+export const removePlaylist = (name) => ({
+  type: constants.REMOVE_PLAYLIST,
+  name
+});
--- app/logic/actions.spec.jsx
+++ app/logic/actions.spec.jsx
@@ -2,11 +2,59 @@ import * as actions from './actions';
 
 describe('addSong action', () => {
     it('should set action type to add song', () => {
-        expect(actions.addSong('Marry go round', 'Popular singers', 1901)).toEqual({
+        expect(actions.addSong('Merry go round', 'Popular singers', 1901)).toEqual({
             type: 'add song',
-            title: 'Marry go round',
+            title: 'Merry go round',
             artist: 'Popular singers',
             year: 1901
+        });
+    });
+});
+
+describe('removeSong action', () => {
+    it('should set action type to remove song', () => {
+        expect(actions.removeSong('Merry go round', 'Popular singers', 1901)).toEqual({
+            type: 'remove song',
+            title: 'Merry go round',
+            artist: 'Popular singers',
+            year: 1901
+        });
+    });
+});
+describe('removeSongGlobally action', () => {
+    it('should set action type to remove song', () => {
+        expect(actions.removeSongGlobally('Merry go round', 'Popular singers', 1901)).toEqual({
+            type: 'remove song globally',
+            title: 'Merry go round',
+            artist: 'Popular singers',
+            year: 1901
+        });
+    });
+});
+
+describe('addPlaylist action', () => {
+    it('should set action type to add playlist', () => {
+        expect(actions.addPlaylist('Playlist name')).toEqual({
+            type: 'add playlist',
+            name: 'Playlist name'
+        });
+    });
+});
+
+describe('selectPlaylist action', () => {
+    it('should set action type to select playlist', () => {
+        expect(actions.selectPlaylist('playlist 1')).toEqual({
+            type: 'select playlist',
+            name: 'playlist 1'
+        });
+    });
+});
+
+describe('removePlaylist action', () => {
+    it('should set action type to remove playlist', () => {
+        expect(actions.removePlaylist('playlist 1')).toEqual({
+            type: 'remove playlist',
+            name: 'playlist 1'
         });
     });
 });
